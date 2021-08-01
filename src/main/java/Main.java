3a85d18dadd5aea50a3e53d44f8339dfbdddbb2b
konstantin77786 <konstantin77786@gmail.com>
2021-08-01 12:43:19 +0300

    First commit Study(Animal)

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import overload.Animal;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal cat = new Animal(10, "black", new ArrayList<Animal>());
+        Animal dog = new Animal();
+    }
+}
